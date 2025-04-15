21cefb1575b8ee8efbee94d23c97854962ceb13c
Arelyaad <aesm130@gmail.com>
2025-04-15 04:14:35 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,31 @@ export default {
     extend: {
       colors: {
         athenia: {
-          50: '#E6EEF4',  // Fondo sutil claro
-          100: '#D2DFEB', // Azul más suave
-          200: '#7D98B2', // Azul medio
-          300: '#406683', // Azul intenso
-          400: '#1B2A41', // Azul oscuro (titulares)
+          50: '#E6EEF4',
+          100: '#D2DFEB',
+          200: '#7D98B2',
+          300: '#406683',
+          400: '#1B2A41',
         },
       },
       backdropBlur: {
-        'xs': '2px',
+        xs: '2px',
       },
       fontFamily: {
-        sans: ['Montserrat', 'sans-serif'], // ✅ Se usa por defecto en toda la UI
+        sans: ['Montserrat', 'sans-serif'],
+      },
+      keyframes: {
+        slideInfinite: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
+      animation: {
+        'slide-infinite': 'slideInfinite 40s linear infinite',
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'), // Opcional: mejora visual de formularios
+    require('@tailwindcss/forms'),
   ],
 };
